5d5cf5cc43929298b208c8f3b92c577d8a70558c
Mr-Yash-01 <mr.yashvadukiya.2003@gmail.com>
2025-02-11 21:12:34 +0530

    signout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,101 +1,48 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
-import { IoIosLogOut } from "react-icons/io";
+import React, { useRef, useState } from "react";
+import Image from 'next/image';
 
-interface userData {
-  email: string;
-  name: string;
+interface UserData {
   picture: string;
+  name: string;
+  email: string;
   role: string;
 }
 
+
 export default function Dashboard({ children }: { children: React.ReactNode }) {
-  const [dropdownVisible, setDropdownVisible] = useState(false);
-  const [position, setPosition] = useState({ top: 0, left: 0 });
+
   const imageRef = useRef<HTMLImageElement>(null);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const [userData, setUserData] = useState<userData>();
 
-  const toggleDropdown = () => {
-    setDropdownVisible((prev) => !prev);
-  };
-
-  useEffect(() => {
-    if (dropdownVisible && imageRef.current && dropdownRef.current) {
-      const buttonRect = imageRef.current.getBoundingClientRect();
-      const dropdownRect = dropdownRef.current.getBoundingClientRect();
-      const viewportWidth = window.innerWidth;
-
-      let leftPosition = buttonRect.left;
-      if (buttonRect.right + dropdownRect.width > viewportWidth) {
-        leftPosition = viewportWidth - dropdownRect.width - 15; // Shift left if out of bounds
-      }
-
-      setPosition({
-        top: buttonRect.bottom + window.scrollY + 15,
-        left: Math.max(10, leftPosition),
-      });
-
-      const userData = localStorage.getItem("userData");
-      if (userData) {
-        setUserData(JSON.parse(userData));
-      }
-    }
-  }, [dropdownVisible]);
-
-  const handleLogOut = () => {
-    localStorage.clear();
-    sessionStorage.clear();
-    // Optionally, you can redirect the user to the login page or home page
-    window.location.href = "/login";
-  };
+  const [userData, setUserData] = useState<UserData | null>(null);
 
   return (
-    <div className="flex flex-col h-min bg-bg text-text ">
+    <div className="flex flex-col h-min bg-bg text-text">
       {/* Navigation bar */}
       <div className="p-4 flex justify-between items-center w-full sticky top-0 bg-bg text-text shadow-2xl shadow-zinc-800 md:px-20 lg:px-72">
-        <img
+        <Image
           src="/LogoWhite.svg"
           alt="logo"
+          width={100}
+          height={100}
           className="w-1/3 md:w-1/4 lg:w-1/12"
         />
-
-        <div className="flex gap-2 items-center">
-          {/* Account Button */}
-          <img
-            src={userData?.picture || '/logoWhite.png'}
-            ref={imageRef}
-            onClick={toggleDropdown}
-            title="Account"
-            alt="avatar"
-            className="w-8 h-8 object-scale-down rounded-full cursor-pointer outline outline-1 lg:w-12 lg:h-12"
-          />
-
-          <button title="Sign Out" className="flex items-center gap-2" onClick={handleLogOut}>
-            <span className="">
-              <IoIosLogOut className="w-8 h-8" />
-            </span>
-          </button>
-        </div>
-
-        {/* Dropdown Menu */}
-        {dropdownVisible && (
-          <div
-            ref={dropdownRef}
-            className="absolute z-50 w-48 p-4 bg-zinc-950  outline outline-1 rounded-lg"
-            style={{ top: position.top, left: position.left }}
-          >
-            <h6 className="opacity-60">User Name:</h6>
-            <h1 className="text-3xl truncate max-w-max">{userData?.name}</h1>
-            <h6 className="opacity-60">Email:</h6>
-            <h5 className="text-lg truncate max-w-max">{userData?.email}</h5>
-          </div>
-        )}
+        <Image
+          src={userData?.picture || '/logoWhite.png'}
+          ref={imageRef}
+          alt="avatar"
+          width={48}
+          height={48}
+          onClick={() => {
+            window.location.href = '/profile';
+          }}
+          className="w-12 h-12 shadow-all-directions shadow-slate-100 object-scale-down rounded-full cursor-pointer outline outline-1 lg:w-12 lg:h-12"
+        />
       </div>
 
       {/* Main content */}
-      <main className="h-screen"> {children} </main>
+      <main className="h-screen">{children}</main>
     </div>
   );
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import axios from "axios";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import { MdEdit, MdLogout, MdOutlineDone } from "react-icons/md";
+
+interface UserData {
+  picture: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+const ProfilePage: React.FC = () => {
+  const [editName, setEditName] = useState(false);
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const storedUserData = localStorage.getItem("userData");
+    if (storedUserData) {
+      setUserData(JSON.parse(storedUserData));
+    }
+  }, []);
+
+  const handleSignOut = async () => {
+    
+    try {
+
+      const res = await axios.post("http://localhost:5000/auth/signout");
+      console.log(res.data);
+      localStorage.removeItem("userData");
+      sessionStorage.removeItem("userData");
+      window.location.href = "/signin";
+
+    } catch (error) {
+
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col  bg-bg text-text">
+      {/* Navigation bar */}
+      <div className="p-4 flex justify-between items-center w-full sticky top-0 bg-bg text-text shadow-2xl shadow-zinc-800 md:px-20 lg:px-72">
+        <Image
+          src="/LogoWhite.svg"
+          alt="logo"
+          width={100}
+          height={100}
+          className="w-1/3 md:w-1/4 lg:w-1/12"
+        />
+
+        <MdLogout onClick={handleSignOut} className="w-8 h-8" />
+      </div>
+
+      {/* Main content */}
+      <main className="h-screen p-4">
+        {/* profile picture */}
+        <div className="flex justify-center items-center">
+          <div className="w-48 h-48 flex justify-center items-center rounded-full shadow-all-directions shadow-slate-100">
+            {userData && (
+              <Image
+                src={userData.picture || "/logoWhite.png"}
+                alt="User Profile Picture"
+                width={150}
+                height={150}
+                className="rounded-full w-full h-full object-contain"
+                priority={userData.picture ? false : true}
+              />
+            )}
+          </div>
+        </div>
+
+        {/* User data */}
+
+        <div className="flex flex-col px-8 pt-8 ">
+          {/* User Name */}
+          <div className="opacity-50 px-2">User Name</div>
+          {editName ? (
+            <div className="flex justify-between items-center gap-4">
+              <input
+                type="text"
+                value={userData?.name}
+                ref={(input) => input?.focus()}
+                placeholder="Enter your name"
+                className="bg-[#181818] w-full text-2xl p-2 rounded-2xl focus:outline-none focus:shadow-md focus:shadow-slate-700 placeholder:font-body placeholder:text-gray-500"
+                onChange={(e) => {
+                  setUserData({
+                    ...userData!,
+                    name: e.target.value,
+                  });
+                }}
+              />
+              <button
+                type="button"
+                title="Save"
+                onClick={() => {
+                  localStorage.setItem("userData", JSON.stringify(userData));
+                  setEditName(false);
+                }}
+              >
+                <MdOutlineDone className="w-6 h-6" />
+              </button>
+            </div>
+          ) : (
+            <div className="flex justify-between items-center p-2">
+              <h1 className="text-2xl w-full">{userData?.name}</h1>
+              <button title="edit" onClick={() => setEditName(true)}>
+                <MdEdit className="w-5 h-5" />
+              </button>
+            </div>
+          )}
+        </div>
+
+        {/* User Email */}
+        <div className="flex flex-col px-8 mt-2">
+          <div className="opacity-50 px-2">Email</div>
+          <div className="flex justify-between items-center p-2">
+            <h1 className="text-2xl w-full">{userData?.email}</h1>
+          </div>
+        </div>
+
+        {/* User Role */}
+        <div className="flex flex-col px-8 mt-2">
+          <div className="opacity-50 px-2">Role</div>
+          <div className="flex justify-between items-center p-2">
+            <h1 className="text-2xl w-full">{userData?.role}</h1>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default ProfilePage;
